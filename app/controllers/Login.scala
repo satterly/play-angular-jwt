d1bd278e48dec09dd7cd4ecfb7acf58f15c5f300
Nick Satterly <nick.satterly@guardian.co.uk>
2015-12-03 22:20:30 +0000

    Delete redundant login action and rename loginAction

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -13,7 +13,7 @@ import org.joda.time.Duration
 import scala.util.{Try, Success, Failure}
 
 trait AuthActions extends Actions {
-  val loginTarget: Call = routes.Login.loginAction()
+  val loginTarget: Call = routes.Login.login()
   val authConfig = OAuth.googleAuthConfig
 
   def tokenChecker(request: RequestHeader) = {
@@ -51,16 +51,10 @@ object OAuth {
 
 class Login extends Controller with AuthActions {
 
-  def login = Action { implicit request =>
-    println(request.session.toString)
-    val error = request.flash.get("error")
-    Ok(views.html.login(error))
-  }
-
   /*
   Redirect to Google with anti forgery token (that we keep in session storage - note that flashing is NOT secure)
    */
-  def loginAction = Action.async { implicit request =>
+  def login = Action.async { implicit request =>
     println(request.session.toString)
     val antiForgeryToken = GoogleAuth.generateAntiForgeryToken()
     GoogleAuth.redirectToGoogle(OAuth.googleAuthConfig, antiForgeryToken).map {
@@ -68,18 +62,12 @@ class Login extends Controller with AuthActions {
     }
   }
 
-  /*
-  User comes back from Google.
-  We must ensure we have the anti forgery token from the loginAction call and pass this into a verification call which
-  will return a Future[UserIdentity] if the authentication is successful. If unsuccessful then the Future will fail.
-
-   */
   def oauth2Callback = Action.async { implicit request =>
     val config = ConfigFactory.load()
     val session = request.session
     session.get(OAuth.ANTI_FORGERY_KEY) match {
       case None =>
-        Future.successful(Redirect(routes.Login.login()).flashing("error" -> "Anti forgery token missing in session"))
+        Future.successful(Redirect(routes.Application.index()).flashing("error" -> "Anti forgery token missing in session"))
       case Some(token) =>
         GoogleAuth.validatedUserIdentity(OAuth.googleAuthConfig, token).map { identity =>
           // We store the URL a user was trying to get to in the LOGIN_ORIGIN_KEY in AuthAction
@@ -99,7 +87,7 @@ class Login extends Controller with AuthActions {
         } recover {
           case t =>
             // you might want to record login failures here - we just redirect to the login page
-            Redirect(routes.Login.login())
+            Redirect(routes.Application.index())
               .withSession(session - OAuth.ANTI_FORGERY_KEY)
               .flashing("error" -> s"Login failure: ${t.toString}")
         }
--- public/js/app.js
+++ public/js/app.js
@@ -17,68 +17,31 @@ angular.module('modtools', [
     $locationProvider.html5Mode(true).hashPrefix('!');
   }])
 
-//.config(['$httpProvider',
-//  function ($httpProvider) {
-//    $httpProvider.interceptors.push(function ($q, $location) {
-//        return {
-//            'request': function (config) {
-//                config.headers = config.headers || {};
-//                config.headers.Authorization = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJyZWFkbWUifQ.1c8tkXQFRGpYkRy25jg5Q6xm40CJhSkXreQKEQU4h00';
-//                return config;
-//            },
-//            'response': function (response) {
-//                if ($location.hash() != "") {
-//                    console.log("Got id_token!!!")
-//                    console.log($location.hash());
-//                    $location.hash('');
-//                }
-//                //Will only be called for HTTP up to 300
-//                console.log("<300");
-//                console.log(response);
-//                return response;
-//            },
-//            'responseError': function (rejection) {
-//                if(rejection.status === 401) {
-//                    console.log("401 redirect to login");
-//                    $location.path('/loginAction');
-//                }
-//                console.log(rejection);
-//                return $q.reject(rejection);
-//            }
-//        };
-//    });
-//}]);
-
-
-//.config(function Config($httpProvider, jwtInterceptorProvider) {
-//  jwtInterceptorProvider.tokenGetter = function() {
-//    return localStorage.getItem('id_token');
-//  };
-//  $httpProvider.interceptors.push('jwtInterceptor');
-//});
-
 .config(['$httpProvider',
   function ($httpProvider) {
     $httpProvider.interceptors.push(function ($q, $location, $window, $localStorage) {
         return {
             'request': function (config) {
-                console.log('request');
                 config.headers = config.headers || {};
                 if ($localStorage.id_token) {
                     config.headers.Authorization = 'Bearer ' + $localStorage.id_token;
                 } else {
-                    console.log('window location');
-                    $window.location = "/loginAction";
+                    $window.location = "/login";
                 }
                 return config;
             },
             'response': function(response) {
                 if ($location.hash()) {
-                    console.log($location.hash);
                     $localStorage.id_token = $location.hash().split('=')[2];
                     $location.hash('');
                 }
                 return response;
+            },
+            'responseError': function (rejection) {
+                if(rejection.status === 401) {
+                    $window.location = "/login";
+                }
+                return $q.reject(rejection);
             }
         };
     });
