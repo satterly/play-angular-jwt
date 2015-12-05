d861a48fa7a826dd2f46bac2877f661b53cbe8a9
Nick Satterly <nick.satterly@guardian.co.uk>
2015-12-05 15:17:36 +0000

    Add login debug

--- public/js/app.js
+++ public/js/app.js
@@ -22,24 +22,40 @@ angular.module('modtools', [
     $httpProvider.interceptors.push(function ($q, $location, $window, $localStorage) {
         return {
             'request': function (config) {
+                console.log('request', config.url);
                 config.headers = config.headers || {};
                 if ($localStorage.id_token) {
+                    console.log('request: token=true');
                     config.headers.Authorization = 'Bearer ' + $localStorage.id_token;
                 } else {
+                    console.log('request: token=false');
                     $window.location = "/login";
                 }
                 return config;
             },
             'response': function(response) {
+                console.log('response', response);
                 if ($location.hash()) {
-                    $localStorage.id_token = $location.hash().split('=')[2];
+                    console.log('response: hash=true');
+                    angular.forEach($location.hash().split('&'), function (hash) {
+                        var s = hash.split('=');
+                        if (s[0] == 'id_token') {
+                            $localStorage.id_token = s[1];
+                        }
+                    });
                     $location.hash('');
+                } else {
+                    console.log('response: hash=false');
                 }
                 return response;
             },
             'responseError': function (rejection) {
+                console.log('responseError', rejection);
                 if(rejection.status === 401) {
+                    console.log('responseError status=401');
                     $window.location = "/login";
+                } else {
+                    console.log('responseError status!=401');
                 }
                 return $q.reject(rejection);
             }
